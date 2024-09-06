8e8580b147c229b2e932c0c615faf7f6bdf65e36
hp <hp@DESKTOP-INM506G>
2024-09-06 23:51:46 +0530

    Initial commit

---
+++ uploadtogit/src/com/uploadtogit/UploadToGit.java
@@ -0,0 +1,11 @@
+package com.uploadtogit;
+
+public class UploadToGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("upload to github");
+	}
+
+}
