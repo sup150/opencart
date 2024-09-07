bd590338d614a3cf995c0697564245f48e07d938
hp <hp@DESKTOP-INM506G>
2024-09-07 22:43:34 +0530

    commit 1 on 7 sept 2024

---
+++ uploadtogit/src/com/uploadtogit/Demo4.java
@@ -0,0 +1,11 @@
+package com.uploadtogit;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my code uploaded in github successfully");
+	}
+
+}
